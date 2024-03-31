2746b86da09ef31866c1546abb0a437508218bae
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-01 00:05:57 +0600

    nested conditonal

---
+++ Introduction_to_ProgrammingLanguage_C/Week_01/Module_2 - Operators, Conditional Statements/Class/conditional_statement/conditional_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int money;
+    scanf("%d",&money);
+    if(money>= 6500)
+    {
+        printf(" phitron er course kino ! ");
+    }
+    else if(money>=5500)
+    {
+        printf(" programming hero er course kinen");
+    }
+    else if (money>= 3000)
+    {
+        printf("bugRegistance er course kinen");
+    }
+    else
+    {
+        printf(" youtube e dekhen , onk resources ache !! ");
+    }
+    
+}
