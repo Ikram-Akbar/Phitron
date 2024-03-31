228526879f7a2a81324698df14899782fee1d9a9
Ikram-akbar <ikramakbar71@gmail.com>
2024-03-31 23:52:09 +0600

    operator and conditional statement

---
+++ Introduction_to_ProgrammingLanguage_C/Week_01/Module_2 - Operators, Conditional Statements/Class/conditional_statement/conditional.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int balace;
+    scanf("%d",&balace);
+    if(balace>=100){
+        printf(" I will eat burger");
+    }
+    else{
+        printf("ruti khaw ajke !! ");
+    }
+    return 0;
+}
