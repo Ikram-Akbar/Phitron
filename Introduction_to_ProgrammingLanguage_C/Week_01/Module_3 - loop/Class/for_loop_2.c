bdb9bf3178d007eea305122895870240016e5ece
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-03 16:08:53 +0600

    loop

---
+++ Introduction_to_ProgrammingLanguage_C/Week_01/Module_3 - loop/Class/for_loop_2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    for (int i = 0; i <= 20; i++)
+    {
+        printf(" the value of i is : %d \n",i);
+    }
+    
+}
+
+/* 
+type_1: 
+    int i ;
+    for ( i = 0; i <=10; i++)
+    {
+        printf(" Hellon. \n ");
+    }
+    
+type_2:
+    for (int i = 0; i <= 20; i++)
+    {
+        printf(" the value of i is : %d \n",i);
+    }
+
+
+ */
