57117fcee754da5d4ae7f1a00afb13baa44a95b6
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-05 04:46:15 +0600

    codeforce

---
+++ Introduction_to_ProgrammingLanguage_C/Week_01/Module_4 - Assignment/code-force/a.hello.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char s[] ="Programmer";
+    printf("Hello %s",s);
+
+    return 0;
+}
--- Introduction_to_ProgrammingLanguage_C/Week_01/Module_4 - Assignment/problem_1.c
+++ Introduction_to_ProgrammingLanguage_C/Week_01/Module_4 - Assignment/problem_1.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello, world! I am learning C programming language.\n ^_^ Programming is fun and challenging. /\\/\\/\\ \n I want to give my 100%% dedication to learn!\t	I will succeed one day.");
+    printf("Hello, world! I am learning C programming language.\n ^_^ Programming is fun and challenging. /\\/\\/\\ \n I want to give my 100%% dedication to learn!\tI will succeed one day.");
     return 0;
 }
