cea3e8cf1c1d3979af69f4acf905192c2972fd4b
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-27 21:20:27 +0600

    pattern _01

--- Introduction_to_ProgrammingLanguage_C/Week_04/Module_13 - Nested Loop Recap/pattern_1.c
+++ Introduction_to_ProgrammingLanguage_C/Week_04/Module_13 - Nested Loop Recap/pattern_1.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
 
-    for (int i = 0; i < n; i++)
-    {
-       printf("*\n");
+int main() {
+    int n, k = 1;
+    scanf("%d", & n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= k; j++) {
+            printf("*");
+        }
+        printf("\n");
+        k++;
     }
-     
     return 0;
 }
