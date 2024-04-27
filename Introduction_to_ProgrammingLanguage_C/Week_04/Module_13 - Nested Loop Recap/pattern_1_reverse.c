519074e2b3453e50ea13a5f7e766712bb7b023fd
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-27 23:46:49 +0600

    pattern 2 - Piramid up

---
+++ Introduction_to_ProgrammingLanguage_C/Week_04/Module_13 - Nested Loop Recap/pattern_1_reverse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,k;
+    scanf("%d %d",&n,&k);
+
+    for (int i = 1; i <= n; i++)
+    {
+       for (int j = 1; j <= k; j++)
+       {
+            printf("*");
+       }
+       printf("\n");
+       k--;
+       
+    }
+     
+    return 0;
+}
