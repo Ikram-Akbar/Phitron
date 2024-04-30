4ead964ce593905bdb18d3c8c330f127e60602f4
Ikram-akbar <ikramakbar71@gmail.com>
2024-04-30 22:23:19 +0600

    pointer value and memory allocation

--- Introduction_to_ProgrammingLanguage_C/Week_04/Module_16 - Function with Pointers/pointer.c
+++ Introduction_to_ProgrammingLanguage_C/Week_04/Module_16 - Function with Pointers/pointer.c
@@ -1,6 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-     
+    int n = 500;
+    int * ptr = &n;
+    printf("n er address : %p\n",&n);
+    printf("ptr er value : %p\n",ptr);
+    printf("ptr er address : %p\n",&ptr);
+
     return 0;
 }
