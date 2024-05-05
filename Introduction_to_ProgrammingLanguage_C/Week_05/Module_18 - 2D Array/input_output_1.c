af3071d057039cc47dfaded266226c4489b95980
Ikram-akbar <ikramakbar71@gmail.com>
2024-05-06 00:39:59 +0600

    print 2D array

---
+++ Introduction_to_ProgrammingLanguage_C/Week_05/Module_18 - 2D Array/input_output_1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int arr[row][col];
+
+    // input :
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    // output:
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
