9afd1f5620ac3132bf204473fc9a37c6283b1bfe
Ikram-akbar <ikramakbar71@gmail.com>
2024-05-24 19:37:16 +0600

    data type

---
+++ Introduction to C++ for DSA/Week_1/Mod_1 - Basic C++/data_type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    double b;
+    float c;
+    long long int d;
+
+    cin >> a >> b >> c >> d;
+    cout << "int- " << a << endl
+         << "double- " << b << endl
+         << "float- " << c << endl
+         << "lld- " << d << endl;
+    return 0;
+}
