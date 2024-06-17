ca4e9e2bd203e5f11b8f90fda8634b0bd4931b20
Ikram-akbar <ikramakbar71@gmail.com>
2024-06-17 13:16:56 +0600

    Order of 1 complexity

---
+++ Basic Data Structures/Week_01 Introduction To Data Structure/Mod_01 Time and Space Complexity/O_1_complexity.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int x = 10; 
+    int y = 50; 
+    int sum = x+y;
+    int mul = x*y;
+    cout<<sum<<" "<<mul<<endl; 
+    return 0;
+}
