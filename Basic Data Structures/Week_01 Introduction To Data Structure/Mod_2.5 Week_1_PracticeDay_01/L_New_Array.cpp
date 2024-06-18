b41e7b4eebe6f716a03a29f66242f98357898644
Ikram-akbar <ikramakbar71@gmail.com>
2024-06-18 16:53:36 +0600

    concate two array

--- Basic Data Structures/Week_01 Introduction To Data Structure/Mod_2.5 Week_1_PracticeDay_01/L_New_Array.cpp
+++ Basic Data Structures/Week_01 Introduction To Data Structure/Mod_2.5 Week_1_PracticeDay_01/L_New_Array.cpp
@@ -1,8 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int>concatArray(vector<int>a,vector<int>b,vector<int>c)
+{
+  for(int val: b)
+  {
+    c.push_back(val);
+  }
+  for(int val: a)
+  {
+    c.push_back(val);
+  }
+  return c;
+
+};
+
 int main()
 {
-     
+  int n;
+  cin>>n;
+  vector<int>a(n);
+  for(int i=0; i<n; i++)
+  {
+    cin>>a[i];
+  }
+  vector<int>b(n);
+    for(int i=0; i<n; i++)
+  {
+    cin>>b[i];
+  }
+  vector<int>c;
+  c = concatArray(a,b,c);
+  for(int val:c)
+  {
+    cout<<val<<" ";
+  }
+
+
+  
   return 0;
 }
