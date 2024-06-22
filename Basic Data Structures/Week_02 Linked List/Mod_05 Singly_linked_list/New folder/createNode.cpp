268df5f0ea09e05ee05d29f3fe5fe5213ab2ac68
Ikram-akbar <ikramakbar71@gmail.com>
2024-06-23 00:00:48 +0600

    single linked list

---
+++ Basic Data Structures/Week_02 Linked List/Mod_05 Singly_linked_list/New folder/createNode.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node 
+{
+    public:
+        int val;
+        Node* next;
+         
+    Node(int value)
+    {
+        this->val = value;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    return 0;
+}
