8f4ce2a7fa364df2d269331033ea69166b023f1d
Ikram-akbar <ikramakbar71@gmail.com>
2024-06-25 19:24:34 +0600

    two numbers problem in cpp

---
+++ Introduction to C++ for DSA/CodeForce_Problem/Sheet_1/E_Area_of_a_Circle.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double r;
+    cin>>r;
+    double pi = 3.141592653;
+    double area = pi*r*r;
+    cout << fixed << setprecision(9) << area; 
+    return 0;
+}
