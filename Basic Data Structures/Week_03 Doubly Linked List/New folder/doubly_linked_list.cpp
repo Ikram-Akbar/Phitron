75e8c5529ef9172e3e67772a7a4b1326c3b69139
Ikram-akbar <ikramakbar71@gmail.com>
2024-07-10 00:17:22 +0600

    print the linked list - normal and reversed way

--- Basic Data Structures/Week_03 Doubly Linked List/New folder/doubly_linked_list.cpp
+++ Basic Data Structures/Week_03 Doubly Linked List/New folder/doubly_linked_list.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int val;
     Node *next;
@@ -14,8 +15,47 @@ public:
     }
 };
 
+void print_linked_list(Node *head)
+{
+    Node *temp = head;
+    cout<<"Your List are => ";
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+void print_end_to_start(Node* tail)
+{
+    Node *current = tail;
+    while(current != NULL)
+    {
+        cout<<current->val<<" ";
+        current = current->prev;
+    }
+}
+
 int main()
 {
+    Node *head = new Node(2);
+    Node *second_node = new Node(4);
+    Node *third_node = new Node(6);
+    Node *tail = third_node;
 
+    // connection :
+    head->next = second_node;
+    head->prev = NULL;
+
+    second_node->prev = head;
+    second_node->next = third_node;
+
+    third_node->prev = second_node;
+    third_node->next = NULL;
+
+    // print :
+    print_linked_list(head);
+    print_end_to_start(tail);
     return 0;
 }
