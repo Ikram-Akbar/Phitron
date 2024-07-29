c1bf523f60c40d249639858860053e3237dab400
Ikram-akbar <ikramakbar71@gmail.com>
2024-07-29 11:52:39 +0600

    assign modifires in linked list

---
+++ Basic Data Structures/Week_03 Doubly Linked List/Module_10 STL List and Cycle Detection/Revision_290724/list_modi.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int>myList = {10,20,30,40};
+    list <int > newList ;
+    // option 1 : 
+    newList = myList;
+    //option 2 : 
+    newList.assign(myList.begin(), myList.end());
+
+    for(int val: newList)
+    {
+        cout<<val<<" ";
+    }
+     
+    return 0;
+}
