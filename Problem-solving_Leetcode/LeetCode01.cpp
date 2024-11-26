a58b03aeb4c0fbf241c288473583b4c6f25da4b0
Ikram Akbar <ikramakbar71@gmail.com>
2024-11-26 12:08:55 +0600

    leetcode : min subarray sum

---
+++ Problem-solving_Leetcode/LeetCode01.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //decare an empty array of integers;
+    int arr[1000];
+    int l,r;
+    //take inputs for left and right indices
+    cin>>l>>r;
+    //take inputs for the array elements
+    for(int i=l;i<=r;i++)
+        cin>>arr[i];
+
+    // find the minimum subarray based on L and R indices;
+    
+    return 0;
+}
